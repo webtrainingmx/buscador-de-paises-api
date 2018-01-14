bd37ef70ec49f50c57f85f1a70f466e86c49f82a
Alex Arriaga <alex.arriaga.m@gmail.com>
2018-01-14 14:13:44 -0500

    Registrar componentes de Vue para laravel passport

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <h2>User information</h2>
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    You are logged in!
+                        You are logged in!
+
+
+                    </div>
                 </div>
+
+                <h2>Passport</h2>
+                <passport-clients></passport-clients>
+                <passport-authorized-clients></passport-authorized-clients>
+                <passport-personal-access-tokens></passport-personal-access-tokens>
+
             </div>
         </div>
     </div>
-</div>
+    </div>
 @endsection
